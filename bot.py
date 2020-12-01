7361da149151367b8f8278e4a321ff1d13b32311
Banji <khanjani1997@gmail.com>
2020-12-01 16:52:31 +0330

    Interact with Login Button

--- bot.py
+++ bot.py
@@ -4,5 +4,12 @@ from selenium import webdriver
 firefox_browser = webdriver.Firefox()
 
 firefox_browser.get("https://www.instagram.com/")
+
+# Login
+login_link = firefox_browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button")
+login_link.click()
+
 sleep(5)
 firefox_browser.close()
+
+
