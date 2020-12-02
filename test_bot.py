b393d6e82a85c9805bb72168a4fdde4108c3ba47
Banji <khanjani1997@gmail.com>
2020-12-02 14:41:47 +0330

    Completing new codes with DRY principles

--- test_bot.py
+++ test_bot.py
@@ -1,15 +1,28 @@
 import os 
 from time import sleep
+from selenium import webdriver
 
-class LoginPage:
-    def __init__(self, browser):
-        self.browser = browser
-    
-    def login(self, username, password):
+firefox_browser = webdriver.Firefox()
+
+class Login:
+    def __init__(self, firefox_browser):
+        self.firefox_browser = firefox_browser
+        self.firefox_browser.get("https://www.instagra.com")
+        
+            
+    def login_me(self, username, password):
         username_input = self.firefox_browser.find_element_by_name("username")
         password_input = self.firefox_browser.find_element_by_name("password")
-        USERNAME = os.environ.get("INSTA_USER")
-        PASSWORD = os.environ.get("INSTA_PASS")
-        username_input.send_keys(USERNAME)
-        password_input.send_keys(PASSWORD)
-        
+        username_input.send_keys(username)
+        password_input.send_keys(password)
+        login_button = self.firefox_browser.find_element_by_xpath("//button[@type='submit']")
+        login_button.click()
+        sleep(5)
+    
+    
+    
+
+username = os.environ.get("INSTA_USER")
+password = os.environ.get("INSTA_PASS")
+login = Login(firefox_browser)
+login.login_me(username, password)
