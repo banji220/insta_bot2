7778ef837e42b25fea61a96674f4a59056f6f74d
Banji <khanjani1997@gmail.com>
2020-12-03 13:22:58 +0330

    Adding relation bounds and potency ratio to the follow() function

--- instapy_py.py
+++ instapy_py.py
@@ -1,4 +1,5 @@
 import os
+from typing import Counter
 from instapy import InstaPy
 from instapy import smart_run
 from selenium import webdriver
@@ -11,6 +12,22 @@ import requests
 # My Instagram Username and Password
 insta_user = os.environ.get('INSTA_USER')
 insta_pass = os.environ.get('INST_PASS')
-def start_session():
-    config_setting = InstaPy(username=insta_user, password=insta_pass, headless_browser=False, nogui=True, multi_logs=False)
-    return config_setting
+
+# Set my configuration and setting and authentication
+def config_setting():
+    session = InstaPy(username=insta_user, password=insta_pass, headless_browser=False, nogui=True, multi_logs=False)
+    return session
+
+# Follow Users
+def follow():
+    session = config_setting()
+    
+    with smart_run(session):
+        counter = 0 
+        while counter < 5:
+            counter += 1
+            
+            try:
+                # Setting 
+                # potency_ratio == (followers count / following count)| eg. 5000(followers)/4000(following) == 1.25
+                session.set_relationship_bounds(enabled=True, potency_ratio=1.25)
