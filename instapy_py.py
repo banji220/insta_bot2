655bf0424c77c9bb6c7fb16bab14cd28eec9e70e
Banji <khanjani1997@gmail.com>
2020-12-04 15:53:09 +0330

    Unfollow Users function is in the process

--- instapy_py.py
+++ instapy_py.py
@@ -14,8 +14,8 @@ def config_setting():
     session = InstaPy(username=insta_user, password=insta_pass, headless_browser=False, multi_logs=False,).login()
     return session
 
-# Follow Users
-def follow():
+# Follow Users Based on Hashtag
+def follow_hashtag_base():
     session = config_setting()
     
     with smart_run(session):
@@ -27,14 +27,43 @@ def follow():
                 # Setting 
                 # potency_ratio == (followers count / following count)| eg. 5000(followers)/4000(following) == 1.25
                 session.set_relationship_bounds(enabled=True,
-                                                potency_ratio=1.25,
-                                                max_followers=2000,
-                                                min_followers=1000,
-                                                min_following=500)
+                                                potency_ratio=None,
+                                                delimit_by_numbers=True,
+                                                max_followers=8000,
+                                                min_followers=200,
+                                                max_following=5000,
+                                                min_following=100,
+                                                )
                 
                 # Activity
                 session.follow_by_tags(tags=["بورس"], amount=5)
                 
             except Exception as e:
-                print("Error")
-follow()
+                print(traceback.format_exc())
+# Unfollow Users
+def unfollow():
+    session = config_setting()
+    
+    with smart_run(session):
+        try:
+            # Setting
+            session.set_relationship_bounds()
+    
+    
+    
+    
+follow_hashtag_base()
+
+
+
+
+
+
+
+
+# Document 
+# `style`
+# You can choose _unfollow style_ as `"FIFO"` (_First-Input-First-Output_) **OR** `"LIFO"` (_Last-Input-First-Output_) **OR** `"RANDOM"`.
+# * with `"FIFO"`, it will unfollow users _in the **exact** order they are loaded_ (_`"FIFO"` is the default style unless you **change** it_);
+# * with `"LIFO`" it will unfollow users _in the **reverse** order they were loaded_;
+# * with `"RANDOM"` it will unfollow users _in the **shuffled** order_;
